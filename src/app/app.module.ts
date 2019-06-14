cd04f842c0f19c7827f9284244727e4b26db5dcd
JustDaile <daile.alimo1991@hotmail.com>
2019-06-14 16:11:22 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MetronomeComponent } from './components/metronome/metronome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MetronomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/metronome/metronome.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-metronome',
+  templateUrl: './metronome.component.html',
+  styleUrls: ['./metronome.component.css']
+})
+
+export class MetronomeComponent implements OnInit {
+
+  @Input() tempo: number;
+  @Input() beats: number;
+
+  audioContext: AudioContext;
+  oscillator: OscillatorNode;
+  intervalTicker;
+
+  ngOnInit() {
+    console.log('constructing metronome: ' + this.tempo);
+    this.audioContext = new AudioContext();
+    this.oscillator = this.audioContext.createOscillator();
+
+    // setup basic oscillator
+    this.oscillator.type = 'sine'; // this is the default - also square, sawtooth, triangle
+    this.oscillator.frequency.value = 0; // Hz
+
+    const $this = this;
+
+    this.oscillator.start();
+
+    let beat = 0;
+
+    this.intervalTicker = setInterval(() => {
+      console.log('tick on beat ' + beat);
+      if (beat === 0) {
+        $this.oscillator.frequency.value = 440; // Hz
+        beat = this.beats;
+      } else {
+        $this.oscillator.frequency.value = 520; // Hz
+      }
+      $this.oscillator.connect($this.audioContext.destination);
+      beat--;
+      setTimeout(() => {
+        $this.oscillator.disconnect($this.audioContext.destination);
+      }, 200);
+    }, 1000 * 60 / this.tempo);
+  }
+
+}
